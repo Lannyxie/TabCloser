4e94fdc898c4e6c701ba50782e81a22b61dc201b
Lanny Xie <xie.la@husky.neu.edu>
2017-12-31 21:35:09 -0500

    added the ability to close a specific tab

--- popup.js
+++ popup.js
@@ -1,9 +1,34 @@
-$(document).ready(function() {
-  chrome.tabs.query({},function(tabs){
-     tabs.forEach(function(tab){
-
+$(document).ready(() => { /*
+  chrome.tabs.query({},(tabs) => {
+     tabs.forEach((tab) => {
        $("#listOfTabs").append("<li>" + tab.url + "</li>");
      });
-  });
+  }); */
+
+
+  $("#checkBoxes").empty(".a_row");
+  $("#theButton").click(closeTabs);
+  this.displayTabsOpen();
+
+
+
 
 });
+
+var currTabs = [];
+
+function displayTabsOpen() {
+    chrome.tabs.query({},(tabs) => {
+       tabs.forEach(function(tab) {
+         $("#checkBoxes").append("<input type=\"checkbox\" name=\"url\" value=\"" + tab.id + "\">" +
+         tab.url + "<br>");
+         currTabs.push(tab.id);
+       }
+     );
+    });
+}
+
+
+function closeTabs() {
+  chrome.tabs.remove(currTabs[0]);
+}
