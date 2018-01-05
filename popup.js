e945173dd7b7966541fd81026c3ed023f3fe829e
Lanny Xie <xie.la@husky.neu.edu>
2018-01-04 19:30:24 -0500

    added preliminary stuff to remember previously unchecked sites

--- popup.js
+++ popup.js
@@ -9,9 +9,7 @@ $(document).ready(() => {
 
   port.postMessage({request: "data"});
   port.onMessage.addListener(function(msg) {
-    console.log('received msg on popup');
     if (msg.replyType == 'checkedMore') {
-      console.log('received tabs to close on popup');
       receivedMsg(msg.data);
     }
   });
@@ -23,9 +21,9 @@ function displayTabsOpen() {
 
             chrome.tabs.query({},(tabs) => {
                tabs.forEach(function(tab) {
-                 $("#checkBoxes").append("<input type=\"checkbox\" class=\"tabRow\""+
-                 "name=\"url\" value=\"" + tab.id + "\">" + "<img src=" + tab.favIconUrl + " >" +
-                 tab.title + "<br>");
+                 $("#checkBoxes").append("<input type=\"checkbox\" class=\"tabRow\""+ "id=\"" + tab.id + "\" " +
+                 "name=\"url\" value=\"" + tab.id + "\" onchange=\"manualCheck(this)\">" +
+                 "<label for=\"" + tab.id + "\">" + "<img class=\"favIcons\" src=" + tab.favIconUrl + " >" + tab.title + "</label>" +"<br>");
                }
              );
             });
@@ -33,9 +31,16 @@ function displayTabsOpen() {
 
 }
 
+function manualCheck(box) {
+
+}
+
+function manuallyChecked(boxID) {
+  return true;
+}
+
 function receivedMsg(tabsToCheck) {
   tabsToCheck.forEach(function(tab_id) {
-    console.log("recieved tab " + tab_id);
     let selector = "input[type=checkbox][value=" + tab_id + "]";
     $(selector).prop("checked", true);
   });
@@ -44,11 +49,19 @@ function receivedMsg(tabsToCheck) {
 
 
 function closeTabs() {
-  let curr = 1;
-  $(".tabRow:checked").each(function() {
-      setTimeout(() => chrome.tabs.remove(parseInt($(this).val())), pace * curr);
-      curr += 1;
+
+  let set = $(".tabRow:checked");
+  let sz = set.length;
+  set.each(function(idx, element) {
+    var boxID = parseInt($(this).val());
+    if (manuallyChecked(boxID)) {
+      setTimeout(() => chrome.tabs.remove(boxID), pace * idx);
+      if (sz - 1 <= idx) {
+        setTimeout(() => {window.close();}, pace * idx + 1)
+      }
+    }
   });
+  // close the popup window after finished closing tabs
 
-
+//alert('Sucessfully closed ' + sz + ' tabs!');
 }
